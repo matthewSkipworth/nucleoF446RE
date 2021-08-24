021e513dd7292bc0a44cd78280d2810eca3ae162
Matthew Skipworth <mfskipwo@uw.edu>
2021-08-23 20:45:02 -0700

    Update stm32f446re_i2c_driver.c

--- source/stm32f446re_i2c_driver.c
+++ source/stm32f446re_i2c_driver.c
@@ -54,9 +54,60 @@ void I2C_Init(I2C_Handle_t* pHandle)
     //4 enable the Acking.
     //5 configure the rise time fo the i2C pins
 
-    
+
 }
 
+uint32_t RCC_GetPLLOutputClock(void)
+{
+    return;
+}
+
+/**
+ *  0xxx: system clock not divided
+    1000: system clock divided by 2
+    1001: system clock divided by 4
+    1010: system clock divided by 8
+    1011: system clock divided by 16
+    1100: system clock divided by 64
+    1101: system clock divided by 128
+    1110: system clock divided by 256
+    1111: system clock divided by 512
+*/
+
+const uint16_t AHB_Prescaler[] = {2, 4, 8, 16, 64, 128, 256, 512};
+const uint8_t APB_Prescalar[] = {2, 4, 8, 16};
+
+uint32_t RCC_getPCLK1Value(void)
+{
+    uint32_t pclk1, SystemClk;
+    uint8_t clksrc, temp, ahpb;
+
+    clksrc = (RCC->CFGR >> 2) & 0x3);
+    if (clksrc == 0)
+    {
+        SystemClk = 16000000
+    }
+    else if (clksrc == 1)
+    {
+        SystemClk = 8000000;
+    }
+    else if (clksrc == 2)
+    {
+        SystemClk = RCC_GetPLLOutputClock();
+    }
+
+    temp = ((RCC ->CFGR >> 4) & 0x0F);
+
+    if (temp < 8)
+    {
+        ahpb = 1;
+    }
+    else 
+    {
+        ahpb = AHB_Prescaler[temp-1];
+    }
+    return pclk1;
+}
 
 /**
  * typedef struct 
